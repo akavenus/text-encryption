2739bc38273583d2e33b7e51cc3e1456c0007b1b
akavenus <71657809+akavenus@users.noreply.github.com>
2022-01-15 19:20:39 -0500

    50

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-//imports
+//module imports
 const express = require('express');
 const bodyParser = require('body-parser'); 
+
+//global functions
 const encryptFile = require('./strategy/encrypt.js')
 const decryptFile = require('./strategy/decrypt.js')
-//global stuff
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+//global variables
 const app = express()
 app.use(bodyParser.json()); 
 const colors = require('colors')
@@ -27,9 +43,16 @@ app.post('/decryptMessage', async function (req, res) {
     var decryptedResponse = await decryptFile.decrypt(req.body.message.split(","), req.body.num1)
     console.log(`|`.blue, `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`.cyan, `Message Decrypted`.blue)
     console.log(`|`.blue, '----------------------------------------------------')
-    return res.send({
-        decryptedResponse: decryptedResponse
-    });
+    if (!decryptedResponse.trim() || decryptedResponse === null){
+        return res.send({
+            decryptedResponse: makeid(rand(100, 500))
+        });
+    }
+    else {
+        return res.send({
+            decryptedResponse: decryptedResponse
+        });
+    }
 });
 //start
 app.listen(PORT, () => {
